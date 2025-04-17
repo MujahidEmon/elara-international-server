64a2464c62948892cfecb947cf57c088a31b016a
MujahidEmon <mujahidulislam.official@gmail.com>
2025-04-18 00:20:39 +0600

    added the function for post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -8,13 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// elara-admin
-// pgUenmzY1SyAiqND
 
 
 
 
-const uri = "mongodb+srv://elara-admin:pgUenmzY1SyAiqND@cluster0.sltxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sltxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,6 +32,26 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const ProductCollection = client.db('ElaraProductDB').collection('products')
+
+
+    // read data from database
+    app.get('/products', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+  })
+
+
+  // Add Products to Database
+  app.post('/products', async (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    const result = await ProductCollection.insertOne(newProduct)
+        res.send(result);
+  })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
