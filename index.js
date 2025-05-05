85337914e7a663e93be0093c6b8dd6ff1b4d3450
MujahidEmon <mujahidulislam.official@gmail.com>
2025-05-06 02:56:13 +0600

    added function to load single product

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -34,13 +34,23 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const ProductCollection = client.db('ElaraProductDB').collection('products')
-
+    const usersCollection = client.db('ElaraProductDB').collection('users')
+    const ordersCollection = client.db('ElaraProductDB').collection('orders')
+    const cartProducts = client.db('ElaraProductDB').collection('cartProducts')
 
     // read data from database
     app.get('/products', async (req, res) => {
       const cursor = ProductCollection.find();
       const result = await cursor.toArray();
       res.send(result)
+  })
+
+  // reading single product
+  app.get('/products/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await ProductCollection.findOne(query);
+    res.send(result);
   })
 
 
@@ -52,6 +62,66 @@ async function run() {
         res.send(result);
   })
 
+
+  app.get('/users', async(req, res) => {
+    const cursor = usersCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+  })
+
+  // for order
+
+  app.get('/orders', async(req, res) => {
+    const cursor = ordersCollection.find();
+    const result = await cursor.toArray()
+    res.send(result);
+  })
+
+  app.post('/users', async (req, res) => {
+    const newUser = req.body;
+    console.log(newUser);
+    const result = await usersCollection.insertOne(newUser)
+    res.send(result)
+  })
+
+  // orders
+  app.post('/orders', async(req, res) => {
+    const newOrder = req.body;
+    console.log(newOrder);
+    const result = await ordersCollection.insertOne(newOrder);
+    res.send(result)
+  })
+
+
+  // Reading Single Order From DB
+  app.get('/orders/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await ordersCollection.findOne(query)
+    res.send(result)
+})
+
+  // Update orders
+  app.put('/orders/:id', async(req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)}
+    const options = {upsert : true}
+    const updatedOrder = req.body
+    const order = {
+        $set: {
+            name: updatedOrder.name,
+            email: updatedOrder.email,
+            phone: updatedOrder.phone,
+            address: updatedOrder.address,
+            note: updatedOrder.category,
+            grandTotal: updatedOrder.grandTotal,
+            // photo: updatedOrder.photo
+        }
+    }
+    const result = await ordersCollection.updateOne(filter, order, options)
+    res.send(result)
+})
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,7 +131,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('elara-int server is runnig');
+    res.send('elara-int server is running');
 })
 
 app.listen(port, () => {
