8598cbcb9cef38a07d3ec48ca5ac86e9886f54f9
MujahidEmon <mujahidulislam.official@gmail.com>
2025-05-25 21:20:48 +0600

    added admin site for cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://elara-international.web.app", "http://localhost:5173", "https://elara-int-admin.web.app"],
+    origin: ["https://elara-international.web.app", "http://localhost:5173" , "http://localhost:5174", "https://elara-int-admin.web.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
